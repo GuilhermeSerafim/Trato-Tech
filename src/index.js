d2934d6e6f670d17c9e7a810efab2fb003dc0c58
GuilhermeSerafim <guilerstudies@gmail.com>
2024-05-15 14:52:16 -0300

    Instalando react-redux e aprendendo todos os conceitos - store, state, view, actions, dispatcher e reducer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from 'routes';
+import store from "./store"
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}> {/* Provider está passando todos os states para os componentes que estão intrínseco */}
+      <Router />
+    </Provider>
   </React.StrictMode>
 );
