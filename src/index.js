b83013c5ec4916b2e1eed7aff43364ab41fd5872
GuilhermeSerafim <guilerstudies@gmail.com>
2024-05-15 13:43:17 -0300

    Finalizando parte de estilos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NavBar from './components/NavBar';
+import Router from 'routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar/>
+    <Router />
   </React.StrictMode>
 );
